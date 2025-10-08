1d1be7e2d9d61fa6fb6d9109073010f539f93361
Joe D'Alessandro <xupit3r@fastmail.com>
2025-10-08 16:52:36 -0400

    some updates n'at

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import puppeteer from 'puppeteer';
+import Nano  from 'nano';
+
+const nano = Nano('http://admin:admin@127.0.0.1:5984');
+
 
 async function getFragranceNotes (page) {
   const TOP_NOTES = 'Top Notes'
   const MIDDLE_NOTES = 'Middle Notes';
   const BASE_NOTES = 'Base Notes';
 
-  const pyramid = await page.$('#pyramid');
-  const text = await page.evaluate(el => el.innerText, pyramid);
+  const text = await page.$eval('#pyramid', result => {
+    return result.innerText
+  });
   const tokens = text.split('\n').map(t => t.trim()).slice(4);
 
   const mnIdx = tokens.indexOf(MIDDLE_NOTES);
@@ -41,20 +46,20 @@ async function getFrangranceDescription (page) {
   return await page.$eval('.fragrantica-blockquote', q => q.innerText.trim());
 }
 
-async function getPageInfo (browser, url) {
-  const page = await browser.newPage();
- 
+async function getPageInfo (page, url) {
   await page.goto(url);
+
+  await delay(3000);
 
   const notes = await getFragranceNotes(page);
   const accords = await getFrangranceAccords(page);
   const description = await getFrangranceDescription(page);
 
-  console.log('-- results --');
-  console.log(notes);
-  console.log(accords);
-  console.log(description);
-  console.log('-------------');
+  return {
+    notes,
+    accords,
+    description
+  }
 }
 
 async function collectLinks (page) {
@@ -85,29 +90,71 @@ function delay (time) {
 }
 
 async function getNotes (page) {
+  const notesDb = nano.use('notes');
+
   await page.goto("https://www.fragrantica.com/notes/");
 
-  return page.$$eval('.notebox a', results => {
+  const notes = await page.$$eval('.notebox a', results => {
     return results.map(el => {
       return {
         note: el.innerText.trim(),
         url: el.href
       }
     });
-  }); 
+  });
+
+  await notesDb.bulk({
+    docs: notes
+  });
+
+  return notes;
+}
+
+async function getScentsByNote (page, url) {
+  await page.goto(url);
+
+  const scents = await page.$$eval('.prefumeHbox a', results => {
+    return results.map(el => {
+      return {
+        name: el.innerText.trim(),
+        url: el.href
+      };
+    });
+  });
+
+  const infos = []
+  for (let i = 0; i < scents.length; i++) {
+    const info = await getPageInfo(page, scents[i].url);
+    info.url = scent[i].url;
+    info.name = scent[i].name;
+
+    infos.push(info);
+  }
+
+  return infos;
+}
+
+async function readNotes () {
+  const notesDb = nano.use('notes');
+  const results = await notesDb.list({ include_docs: true });
+
+  return results.rows.map(({ doc }) => doc);
 }
 
 async function run () {
   const browser = await puppeteer.launch({
     headless: false
   });
+
   const page = await browser.newPage();
-  
   await page.setViewport({width: 1080, height: 1024});
 
-  const notes = await getNotes(page);
+  const scents = await getScentsByNote(
+    page,
+    'https://www.fragrantica.com/notes/Apple-146.html'
+  );
 
-  console.log(notes);
+  console.log(scents);
 
   await browser.close();
 }
